790b75dc118427a1a6be42cb6807194309ddf745
YelyseiShcherbak <yelysei@roomreview.co.uk>
2026-09-21 22:36:00 +0200

    Add files via upload

---
+++ src/config/boroughImages.ts
@@ -0,0 +1,71 @@
+import barkingAndDagenhamImage from '@img/Barking and Dagenham.png';
+import barnetImage from '@img/Barnet.png';
+import bexleyImage from '@img/Bexley.jpg';
+import brentImage from '@img/Brent.jpg';
+import bromleyImage from '@img/Bromley.jpg';
+import camdenImage from '@img/Camden.jpg';
+import cityOfLondonImage from '@img/city-london.jpg';
+import croydonImage from '@img/Croydon.png';
+import ealingImage from '@img/Ealing.png';
+import enfieldImage from '@img/Enfield.jpg';
+import greenwichImage from '@img/Greenwich.jpg';
+import hackneyImage from '@img/Hackney.png';
+import hammersmithAndFulhamImage from '@img/Hammersmith and Fulham.jpg';
+import haringeyImage from '@img/Haringey.jpg';
+import harrowImage from '@img/Harrow.jpg';
+import haveringImage from '@img/Havering.png';
+import hillingdonImage from '@img/Hillingdon.jpg';
+import hounslowImage from '@img/Hounslow.jpg';
+import islingtonImage from '@img/Islington.jpg';
+import kensingtonAndChelseaImage from '@img/Kensington and Chelsea.jpg';
+import kingstonUponThamesImage from '@img/Kingston upon Thames.jpg';
+import lambethImage from '@img/Lambeth.jpg';
+import lewishamImage from '@img/Lewisham.png';
+import mertonImage from '@img/Merton.jpg';
+import newhamImage from '@img/Newham.jpg';
+import redbridgeImage from '@img/Redbridge.png';
+import richmondUponThamesImage from '@img/Richmond upon Thames.jpg';
+import southwarkImage from '@img/Southwark.jpg';
+import suttonImage from '@img/Sutton.png';
+import towerHamletsImage from '@img/Tower Hamlets.jpg';
+import walthamForestImage from '@img/Waltham Forest.png';
+import wandsworthImage from '@img/Wandsworth.jpg';
+import westminsterImage from '@img/Westminister.jpg';
+
+const boroughImages: Record<string, string> = {
+  'Barking and Dagenham': barkingAndDagenhamImage,
+  Barnet: barnetImage,
+  Bexley: bexleyImage,
+  Brent: brentImage,
+  Bromley: bromleyImage,
+  Camden: camdenImage,
+  'City of London': cityOfLondonImage,
+  Croydon: croydonImage,
+  Ealing: ealingImage,
+  Enfield: enfieldImage,
+  Greenwich: greenwichImage,
+  Hackney: hackneyImage,
+  'Hammersmith and Fulham': hammersmithAndFulhamImage,
+  Haringey: haringeyImage,
+  Harrow: harrowImage,
+  Havering: haveringImage,
+  Hillingdon: hillingdonImage,
+  Hounslow: hounslowImage,
+  Islington: islingtonImage,
+  'Kensington and Chelsea': kensingtonAndChelseaImage,
+  'Kingston upon Thames': kingstonUponThamesImage,
+  Lambeth: lambethImage,
+  Lewisham: lewishamImage,
+  Merton: mertonImage,
+  Newham: newhamImage,
+  Redbridge: redbridgeImage,
+  'Richmond upon Thames': richmondUponThamesImage,
+  Southwark: southwarkImage,
+  Sutton: suttonImage,
+  'Tower Hamlets': towerHamletsImage,
+  'Waltham Forest': walthamForestImage,
+  Wandsworth: wandsworthImage,
+  Westminster: westminsterImage,
+};
+
+export default boroughImages;
